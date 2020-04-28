c195a118ede901c94a78bf078f34a3b46c0d4a15
Caleb Wiederhold <cjwiederhold@gmail.com>
2020-04-28 16:51:12 -0500

    added comments

--- app.js
+++ app.js
@@ -20,36 +20,47 @@ const checkCall = async (msg) => {
         return undefined;
     }
 
+    // check for valid quote 
     if (!msg.content.match(/"([^"]+)"/)){
         await msg.reply('quote must be between quote marks ("").');
         return undefined;
     }
+
+    // I'm not good with promises, so this is my way of making things work
     return 1;
 }
 
 const createQuote = async (msg) => {
+    // set nickname for quote to be null, only updates if one is provided
     nick = null;
     if(msg.content.match(/\(([^)]+)\)/)) nick = msg.content.match(/\(([^)]+)\)/)[1];
 
+    // creqte quote object
     const quote = new Quote({
         user: msg.mentions.members.first().user.id,
         quote: msg.content.match(/"([^"]+)"/)[1],
         nick
     })
 
+    // save quote to database
     quote.save()
 }
 
 const getQuoteRand = async (msg) => {
+    // function is only called if a quote is not called by nickname
+    // load all quotes from a user
     const quotes = await Quote.find({
         user: msg.mentions.members.first().user.id
     })
-    if (!quotes[0]) return undefined;
+    if (!quotes[0]) return undefined; //break if user has no quotes
+
+    // get random index in range of number of quotes and select
     const rand = (Math.floor(Math.random() * quotes.length))
     return quotes[rand].quote;
 }
 
 const getQuoteNick = async (nick) => {
+    // get quote by nickname if it exists
     const quotes = await Quote.find({
         nick
     })
@@ -64,9 +75,12 @@ client.on('ready', () => {
 client.on('message', msg => {
     guild = msg.guild;
 
+    // check for command syntax of messages, break if not relevant
+    // 70% sure this does what I want it to do
     if (msg.content.charAt(0) !== '!') return;
     const msgArr = msg.content.split(' ');
     
+    // addquote command call
     if (msgArr[0] === '!addquote' && msg.member.roles.cache.find(r => r.name === 'Moderators')) {
         checkCall(msg).then((res, err) => {
             if (res) {
@@ -76,12 +90,15 @@ client.on('message', msg => {
         })
     }
 
+    // find quote command call
     if (msgArr[0] === "!quote") {
+        // find quote when nickname is provided
         if (msg.content.match(/\(([^)]+)\)/)) {
             getQuoteNick(msg.content.match(/\(([^)]+)\)/)).then(res => {
                 if (res) return msg.reply('"' + res + '"');
             })
         }
+        // find quote when no nickname is provided
         if (msg.mentions.members.first()) {
             getQuoteRand(msg).then(res => {
                 if (res) return msg.reply('"' + res + '"');
@@ -92,10 +109,11 @@ client.on('message', msg => {
 
 client.login(process.env.DISCORD_API);
     
-    
+    // This works to insert a quote to DB
+
     //     const myQuote = new Quote({
     //         user: 'cwied',
-    //         quote: 'knobs',
+    //         quote: 'test quote',
     //         nick: 'k'
     //     })
         
